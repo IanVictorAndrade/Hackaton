e65fbcc8b9b92aad92274ed9b58be95efa35ee04
Lucas Costa <lucascostafilho@hotmail.com>
2024-02-22 19:09:22 -0400

    commit

--- Administrador.java
+++ Administrador.java
@@ -1,3 +1,79 @@
+import javax.swing.JOptionPane;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Administrador {
-    
+    private String nome;
+    private String login;
+    private String senha;
+
+    //Construtor
+
+    public Administrador(String nome, String login, String senha) {
+        this.nome = nome;
+        this.login = login;
+        this.senha = senha;
+    }
+
+    //Getters e Setters
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    //Metodos
+
+    public static boolean validarLogin() {
+
+    }
+
+    public static boolean validarSenha() {
+
+    }
+
+    public static String cadastrarProfessor() {
+
+    }
+
+    public static String alterarCadProf() {
+
+    }
+
+    public static String cadastrarDisciplina() {
+
+    }
+
+    public static String alterarCadDisc() {
+
+    }
+
+    public static String cadastrarTurma() {
+
+    }
+
+    public static String alterarCadTurma() {
+        
+    }
+
 }
--- Disciplina.java
+++ Disciplina.java
@@ -1,3 +1,49 @@
+import javax.swing.JOptionPane;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Disciplina {
+    private String nomeDisciplina;
+    private int cargaHorariaS;
+    private String professorResponsavel;
     
+    //Construtor
+    
+    public Disciplina(String nomeDisciplina, int cargaHorariaS) {
+        this.nomeDisciplina = nomeDisciplina;
+        this.cargaHorariaS = cargaHorariaS;
+    }
+
+    //Getters e Setters
+
+    public String getNomeDisciplina() {
+        return nomeDisciplina;
+    }
+    public void setNomeDisciplina(String nomeDisciplina) {
+        this.nomeDisciplina = nomeDisciplina;
+    }
+    public int getCargaHorariaS() {
+        return cargaHorariaS;
+    }
+    public void setCargaHorariaS(int cargaHorariaS) {
+        this.cargaHorariaS = cargaHorariaS;
+    }
+    public String getProfessorResponsavel() {
+        return professorResponsavel;
+    }
+    public void setProfessorResponsavel(String professorResponsavel) {
+        this.professorResponsavel = professorResponsavel;
+    }
+
+    //Metodos
+
+    public static String listarDisciplinas() {
+
+    }
+
+    public static String exibirChoque() {
+
+    }
+
 }
